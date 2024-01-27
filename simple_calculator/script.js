4bf69b096521b606bd266941a49961e521aeb07e
papasmurfuwu <quentanwong@gmail.com>
2024-01-28 00:59:57 +0800

    Added JS for buttons

---
+++ simple_calculator/script.js
@@ -0,0 +1,18 @@
+// 1. bind each button to corresponding function
+// 2. store input into string 
+// 3. function() turn string into integer => return product! 
+
+let numDisplay = document.getElementById('display');
+
+
+function appendToDisplay(str){
+    display.value += str;
+} 
+
+function clearDisplay(){
+    display.value = '';
+}
+
+function calculate(){
+    display.value = eval(display.value);
+}
