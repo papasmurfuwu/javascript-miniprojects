1ffbd60f0c3ab838540a4e3ff9d52841af73aa51
papasmurfuwu <quentanwong@gmail.com>
2024-02-01 23:01:27 +0800

    Slightly modified CSS for expense-input width, also added Javascript

---
+++ expense-tracker/script.js
@@ -0,0 +1,16 @@
+// 1. learn how add date select (open calender feature)
+// 2. Add function to "Add Expense Button"
+// 
+
+
+
+function addExpense(){ 
+    let noExpenseMessage = document.getElementById('no-expense-msg'); 
+    let addExpenseButton = document.getElementById('add-expense-button');
+
+    addExpenseButton.addEventListener('click', function(){
+        noExpenseMessage.style.
+    })
+
+
+}
