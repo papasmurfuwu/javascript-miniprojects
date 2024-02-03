cd94d597d445d07606405d502671f18283ddb203
papasmurfuwu <quentanwong@gmail.com>
2024-02-04 00:19:21 +0800

    Added CSS grid for pictures

---
+++ memory-game/script.js
@@ -0,0 +1,7 @@
+// 1. figure out how to cover images
+// 2. on click show picture 
+// 3. create matching mechanism 
+// 
+// --> using class, if classes of 2 selected pics match
+// --> add a point  
+// Additional: randomise order each time
